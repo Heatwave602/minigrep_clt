7aa6152b63f21ea6405dfbb6bde862764cce6096
Marco Afonso Albino Ramião <marcoafonso1706@gmail.com>
2026-07-24 15:23:00 +0100

    Improved error handling in main. Also created a new run function to handle all logic that was done in main, apart from parsing arguments and handling errors.

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,34 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 use std::{env};
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {panic!("Invalid number of arguments passed: expecting 2");}
 
-    let cfg = Config::new(&args);
+    // parser cl arguments
+    let config = Config::build(&args).
+        unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        });
 
-    let Ok(it) = read_lines(cfg.file_path) else {
-        todo!("handle IO errors");
+    // error handling
+    if let Err(err) = run(&config) {
+        println!("Application error: {err}");
+        process::exit(1);
     };
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let it = read_lines(config.file_path)?;
 
     for line in it {
         println!("{}", line.unwrap());
     }
-}
 
-fn read_lines<P>(path: P) -> io::Result<io::Lines<io::BufReader<File>>> 
-where P: AsRef<Path> {
-    let file = File::open(path)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(())
 }
 
 struct Config<'a> {
@@ -31,10 +39,19 @@ struct Config<'a> {
 }
 
 impl<'a> Config<'a> {
-    fn new(args: &'a [String]) -> Config<'a> {
+    fn build(args: &'a [String]) -> Result<Config<'a>, &'static str> {
+        let len = args.len();
+        if len < 3 {return Err("not enough arguments");}
+
         // let query     = &args[1];
         let file_path = &args[2];
 
-        Config { /*query*/ file_path }
+        Ok(Config { /*query*/ file_path })
     }
+}
+
+fn read_lines<P>(path: P) -> io::Result<io::Lines<io::BufReader<File>>> 
+where P: AsRef<Path> {
+    let file = File::open(path)?;
+    Ok(io::BufReader::new(file).lines())
 }
